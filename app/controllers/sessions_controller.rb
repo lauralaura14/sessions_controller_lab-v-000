f7812fa8401bdcda1950c1fcfb100afcd9ba8a8c
Laura <laurao148@gmail.com>
2018-10-27 02:10:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-      if !params[:name].nil?
-        session[:name] = params[:name]
-        redirect_to '/'
-      else
-        redirect_to '/sessions/new'
-      end
-  end
-
-  def destroy
-    if session[:name]
-      session.delete :name
+    session[:name] = params[:name]
+    if session[:name].blank?
+      redirect_to sessions_new_path
     else
-      session[:name] = nil
+      redirect_to root_path
     end
   end
-
+   def destroy
+    if session[:name].nil?
+      redirect_to root_path
+    else
+      session.delete :name
+      redirect_to root_path
+    end
+  end
 end
