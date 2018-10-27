d24eb120279feb8f37b72c441b8f3aac3dddbb2d
Laura <laurao148@gmail.com>
2018-10-27 00:26:57 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
         session[:name] = params[:name]
         redirect_to '/'
       else
-        redirect_to (controller: 'sessions', action: 'new')
+        redirect_to '/sessions/new'
       end
   end
 
