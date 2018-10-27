65d6ec8387929a69952eff243f837dd8cc493ec3
Laura <laurao148@gmail.com>
2018-10-27 00:27:45 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-      if params[:name].present?
+      if !params[:name].nil?
         session[:name] = params[:name]
         redirect_to '/'
       else
